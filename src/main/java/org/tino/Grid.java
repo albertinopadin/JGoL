8cfd187fc9bfacc1265a5d53f95718416b69d31e
Albertino Padin <albertino.padin@gmail.com>
2023-12-19 04:39:31 -0800

    Full implementation of Grid class

--- src/main/java/org/tino/Grid.java
+++ src/main/java/org/tino/Grid.java
@@ -2,7 +2,7 @@ package org.tino;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -17,11 +17,11 @@ public class Grid {
         this.xCells = xCells;
         this.yCells = yCells;
         this.generation = 0;
-        this.cells = (Cell[]) IntStream.range(0, xCells * yCells).mapToObj(i -> {
+        this.cells = IntStream.range(0, xCells * yCells).mapToObj(i -> {
             int x = i % xCells;
             int y = i / xCells;
             return new Cell(false, new Point2D.Double(x, y), Color.BLUE);
-        }).toArray();
+        }).toArray(Cell[]::new);
         this.setNeighbors();
     }
 
@@ -33,8 +33,92 @@ public class Grid {
         }
     }
 
-    // TODO: Implement
     private List<Cell> getNeighborsForCell(int x, int y) {
-        return Collections.emptyList();
+        ArrayList<Cell> neighbors = new ArrayList<>();
+
+        int leftX = x - 1;
+        int rightX = x + 1;
+        int topY = y - 1;
+        int bottomY = y + 1;
+
+        if (leftX > -1) {
+            neighbors.add(this.cells[leftX + y&this.xCells]);
+        }
+
+        if (leftX > -1 && topY > -1) {
+            neighbors.add(this.cells[leftX + topY*this.xCells]);
+        }
+
+        if (topY > -1) {
+            neighbors.add(this.cells[x + topY*this.xCells]);
+        }
+
+        if (rightX < this.xCells && topY > -1) {
+            neighbors.add(this.cells[rightX + topY*this.xCells]);
+        }
+
+        if (rightX < this.xCells) {
+            neighbors.add(this.cells[rightX + y*this.xCells]);
+        }
+
+        if (rightX < this.xCells && bottomY < this.yCells) {
+            neighbors.add(this.cells[rightX + bottomY*this.xCells]);
+        }
+
+        if (bottomY < this.yCells) {
+            neighbors.add(this.cells[x + bottomY*this.xCells]);
+        }
+
+        if (leftX > -1 && bottomY < this.yCells) {
+            neighbors.add(this.cells[leftX + bottomY*this.xCells]);
+        }
+
+        return neighbors;
+    }
+
+    public int update() {
+        for (int x = 0; x < this.xCells; x++) {
+            for (int y = 0; y < this.yCells; y++) {
+                this.cells[x + y*this.xCells].prepareUpdate();
+            }
+        }
+
+        for (int x = 0; x < this.xCells; x++) {
+            for (int y = 0; y < this.yCells; y++) {
+                this.cells[x + y*this.xCells].update();
+            }
+        }
+
+        ++this.generation;
+        return this.generation;
+    }
+
+    public void reset() {
+        for (int x = 0; x < this.xCells; x++) {
+            for (int y = 0; y < this.yCells; y++) {
+                this.cells[x + y*this.xCells].makeDead();
+            }
+        }
+
+        this.generation = 0;
+    }
+
+    public void makeAllLive() {
+        for (int x = 0; x < this.xCells; x++) {
+            for (int y = 0; y < this.yCells; y++) {
+                this.cells[x + y*this.xCells].makeLive();
+            }
+        }
+    }
+
+    public void randomState(Double liveProbability) {
+        this.reset();
+        for (int x = 0; x < this.xCells; x++) {
+            for (int y = 0; y < this.yCells; y++) {
+                if (Math.random() <= liveProbability) {
+                    this.cells[x + y * this.xCells].makeLive();
+                }
+            }
+        }
     }
 }
